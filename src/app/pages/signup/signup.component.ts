4c9902332a344076f8eabe2f0421e42d44fdee0e
celsogl <celsogonzalesleon@gmail.com>
2023-11-15 02:14:48 -0500

    update

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  
+
+ 
+ public user = {
+  usuario : '',
+  pass: ''
+ }
+
+ constructor ( private userService: UserService){}
+
+  ngOnInit(): void {
+  }
+
+  formSubmit(){
+    console.log(this.user);
+    if(this.user.usuario==''|| this.user.usuario == null){
+      alert('El nombre de usuario es requerido.');
+      return;
+    }
+
+    this.userService.saveUsuario(this.user).subscribe(
+      (data) => {
+        console.log(data)
+        alert('Usuario guardado con éxito.');
+      },(error) => {
+        console.log(error);
+        alert('Ha ocurrido un error en el sistema.')
+      }
+    )
+
+  }
+
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import baseUrl from './helper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor( private httpClient: HttpClient) { }
+
+  public saveUsuario(user:any){
+
+    return this.httpClient.post(`${baseUrl}/usuarios/`,user);
+
+  }
 }
